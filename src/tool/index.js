d4506f80f18a3f7c39ee50cecf28195779f7f77a
zecoo89 <[omitted]>
2019-01-06 18:41:27 +0900

    Fix

--- src/tool/index.js
+++ src/tool/index.js
@@ -2,11 +2,15 @@ import Ppu from '../ppu'
 import Renderer from '../renderer'
 
 export default class Tool {
-  static dumpChrRom(rom, id, palette) {
+  constructor(rom) {
+    this.rom = rom
+  }
+
+  dumpChrRom(id, palette) {
     const ppu = new Ppu()
     const renderer = new Renderer(id)
 
-    ppu.chrRom = rom.chrRom
+    ppu.chrRom = this.rom.chrRom
     const tiles = ppu.tiles
 
     let x = 0,
