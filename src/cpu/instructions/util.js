bfd8f9417f92d630770c374bb24dad2d4082db44
zecoo89 <[omitted]>
2019-01-09 03:35:27 +0900

    Add function

--- src/cpu/instructions/util.js
+++ src/cpu/instructions/util.js
@@ -1,6 +1,7 @@
 export default {
   isNegative: value => value >> 7,
   isZero: value => (value === 0x00) & 1,
+  isRam: addr => addr !== null && addr !== undefined,
   msb: value => value >> 7,
   lsb: value => value & 0x01,
   add: (a, b) => (a + b) & 0xff,
