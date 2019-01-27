80d61a2c665ebaabab18cc301e17d11da6b67fe3
zecoo89 <[omitted]>
2019-01-27 11:37:24 +0900

    add all-in-one mode

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import Rom_ from './rom'
 import Screen_ from './screen'
 import Audio_ from './audio'
 import Tool_ from './tool'
+import { AllInOne as AllInOne_ } from './nes'
 
 export const Nes = Nes_
 export const Rom = Rom_
 export const Screen = Screen_
 export const Audio = Audio_
 export const Tool = Tool_
+export const AllInOne = AllInOne_
+
--- src/nes.js
+++ src/nes.js
@@ -2,6 +2,9 @@ import Cpu from './cpu'
 import Ppu from './ppu'
 import Apu from './apu'
 import Controller from './controller'
+import Screen from './screen'
+import Rom from './rom'
+import { isNodejs } from './utils'
 
 export default class Nes {
   constructor(isDebug) {
@@ -34,5 +37,50 @@ export default class Nes {
     this.ppu.chrRom = this.rom.chrRom
 
     this.cpu.run()
+  }
+}
+
+export class AllInOne {
+  constructor(screenId, isDebug) {
+    const screen = new Screen.Browser(screenId)
+    const audio = new Audio()
+
+    this.nes = new Nes(isDebug)
+    this.nes.connect({
+      screen,
+      audio
+    })
+  }
+
+  async run(romPath) {
+    if(typeof romPath === 'string') {
+      const data = isNodejs() ? await this.readFile(romPath) : await this.download(romPath)
+      const rom = new Rom(data)
+      this.nes.rom = rom
+    } else if(romPath.constructor.name === 'Rom') {
+      this.nes.rom = romPath
+    } else {
+      throw new Error()
+    }
+
+    this.nes.run()
+  }
+
+  async download(romUrl) {
+    const data = await fetch(romUrl)
+    .then(response => response.arrayBuffer())
+    .then(buffer => new Uint8Array(buffer))
+
+    return data
+  }
+
+  async readFile(romPath) {
+    const fs = require('fs')
+    const util = require('util')
+
+    const readFile = util.promisify(fs.readFile)
+    const data = await readFile(romPath)
+
+    return data
   }
 }
