d58bd0d4d6fd363f7cced9e2d876f9f59ea3fd30
zecoo <10446970+zecoo89@users.noreply.github.com>
2019-02-26 22:16:55 +0900

    Fix comments

--- src/cpu/opcodes/0x4x.js
+++ src/cpu/opcodes/0x4x.js
@@ -6,7 +6,7 @@ export default [
   { instruction: 'EOR', addressing: 'indexIndirect', cycles: 6 },
   /* TODO This is not implemented */
   { instruction: 'STP', addressing: 'implied', cycles: 3 },
-  /* TODO This is not implemented */
+  /* 0x43: SRE indexIndirect */
   { instruction: 'SRE', addressing: 'indexIndirect', cycles: 8 },
   /* 0x44: 2byte NOP (Use zeropage for 2byte}*/
   { instruction: 'NOP', addressing: 'zeropage', cycles: 3 },
@@ -14,7 +14,7 @@ export default [
   { instruction: 'EOR', addressing: 'zeropage', cycles: 3 },
   /* 0x46: LSR zeropage*/
   { instruction: 'LSR', addressing: 'zeropage', cycles: 5 },
-  /* TODO This is not implemented */
+  /* 0x47: SRE zeropage */
   { instruction: 'SRE', addressing: 'zeropage', cycles: 5 },
   /* 0x48: PHA implied */
   { instruction: 'PHA', addressing: 'implied', cycles: 3 },
@@ -22,7 +22,7 @@ export default [
   { instruction: 'EOR', addressing: 'immediate', cycles: 2 },
   /* 0x4a: LSR implied(accumulator} */
   { instruction: 'LSR', addressing: 'implied', cycles: 2 },
-  /* TODO This is not implemented */
+  /* 0x4b: ALR immediate */
   { instruction: 'ALR', addressing: 'immediate', cycles: 2 },
   /* 0x4c: JMP absolute */
   { instruction: 'JMP', addressing: 'absolute', cycles: 3 },
@@ -30,6 +30,6 @@ export default [
   { instruction: 'EOR', addressing: 'absolute', cycles: 4 },
   /* 0x4e: LSR absolute*/
   { instruction: 'LSR', addressing: 'absolute', cycles: 6 },
-  /* TODO This is not implemented */
+  /* 0x4f: SRE absolute */
   { instruction: 'SRE', addressing: 'absolute', cycles: 6 }
 ]
