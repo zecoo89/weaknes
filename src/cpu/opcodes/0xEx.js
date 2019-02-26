8cf412fd8d22cb9a953011c62a60e0322658f030
zecoo <10446970+zecoo89@users.noreply.github.com>
2019-02-26 22:32:50 +0900

    Fix comments

--- src/cpu/opcodes/0xEx.js
+++ src/cpu/opcodes/0xEx.js
@@ -4,17 +4,17 @@ export default [
   { instruction: 'CPX', addressing: 'immediate', cycles: 2 },
   /* 0xe1: SBC indexIndirect */
   { instruction: 'SBC', addressing: 'indexIndirect', cycles: 6 },
-  /* 0xe0: 2byte NOP */
+  /* 0xe2: 2byte NOP */
   { instruction: 'NOP', addressing: 'immediate', cycles: 2 },
-  /* TODO This is not implemented */
+  /* 0xe3: ISC indexIndirect */
   { instruction: 'ISC', addressing: 'indexIndirect', cycles: 8 },
   /* 0xe4: CPX zeropage */
   { instruction: 'CPX', addressing: 'zeropage', cycles: 3 },
-  /* 0xe5: SBC zeropage*/
+  /* 0xe5: SBC zeropage */
   { instruction: 'SBC', addressing: 'zeropage', cycles: 3 },
-  /* 0xe6: INC zeropage*/
+  /* 0xe6: INC zeropage */
   { instruction: 'INC', addressing: 'zeropage', cycles: 5 },
-  /* TODO This is not implemented */
+  /* 0xe7: ISC zeropage */
   { instruction: 'ISC', addressing: 'zeropage', cycles: 5 },
   /* 0xe8: INX implied */
   { instruction: 'INX', addressing: 'implied', cycles: 2 },
@@ -22,14 +22,14 @@ export default [
   { instruction: 'SBC', addressing: 'immediate', cycles: 2 },
   /* 0xea: NOP implied */
   { instruction: 'NOP', addressing: 'implied', cycles: 2 },
-  /* TODO This is not implemented */
+  /* 0xeb: SBC immediate */
   { instruction: 'SBC', addressing: 'immediate', cycles: 2 },
   /* 0xec: CPX absolute */
   { instruction: 'CPX', addressing: 'absolute', cycles: 4 },
   /* 0xed: SBC absolute */
   { instruction: 'SBC', addressing: 'absolute', cycles: 4 },
-  /* 0xee: INC absolute*/
+  /* 0xee: INC absolute */
   { instruction: 'INC', addressing: 'absolute', cycles: 6 },
-  /* TODO This is not implemented */
+  /* 0xef: ISC absolute */
   { instruction: 'ISC', addressing: 'absolute', cycles: 6 }
 ]
