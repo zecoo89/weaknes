5ebd29c696f9a66b8bb17ed183b5f147a3bcc00d
zecoo <10446970+zecoo89@users.noreply.github.com>
2019-02-26 22:19:01 +0900

    Fix comments

--- src/cpu/opcodes/0x5x.js
+++ src/cpu/opcodes/0x5x.js
@@ -6,7 +6,7 @@ export default [
   { instruction: 'EOR', addressing: 'indirectIndex', cycles: 5 },
   /* TODO This is not implemented */
   { instruction: 'STP', addressing: 'implied', cycles: 3 },
-  /* TODO This is not implemented */
+  /* 0x53: SRE indirectIndex */
   { instruction: 'SRE', addressing: 'indirectIndex', cycles: 8 },
   /* 0x54: 2byte NOP zeropageX */
   { instruction: 'NOP', addressing: 'zeropageX', cycles: 4 },
@@ -14,7 +14,7 @@ export default [
   { instruction: 'EOR', addressing: 'zeropageX', cycles: 4 },
   /* 0x56: LSR zeropageX */
   { instruction: 'LSR', addressing: 'zeropageX', cycles: 6 },
-  /* TODO This is not implemented */
+  /* 0x57: SRE zeropageX */
   { instruction: 'SRE', addressing: 'zeropageX', cycles: 6 },
   /* 0x58: CLI */
   { instruction: 'CLI', addressing: 'implied', cycles: 2 },
@@ -22,7 +22,7 @@ export default [
   { instruction: 'EOR', addressing: 'absoluteY', cycles: 4 },
   /* 0x5a: NOP */
   { instruction: 'NOP', addressing: 'implied', cycles: 2 },
-  /* TODO This is not implemented */
+  /* 0x5b: SRE absoluteY */
   { instruction: 'SRE', addressing: 'absoluteY', cycles: 7 },
   /* 0x5c: 3byte NOP (Use absolute for 3byte}*/
   { instruction: 'NOP', addressing: 'absolute', cycles: 4 },
@@ -30,6 +30,6 @@ export default [
   { instruction: 'EOR', addressing: 'absoluteX', cycles: 4 },
   /* 0x5e LSR absoluteX */
   { instruction: 'LSR', addressing: 'absoluteX', cycles: 7 },
-  /* TODO This is not implemented */
+  /* 0x5f SRE absoluteX */
   { instruction: 'SRE', addressing: 'absoluteX', cycles: 7 }
 ]
