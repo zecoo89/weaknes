4c2160cd3da6b7312c97050425f0c0527db52ff2
zecoo <10446970+zecoo89@users.noreply.github.com>
2019-02-26 22:34:19 +0900

    Fix comments

--- src/cpu/opcodes/0xFx.js
+++ src/cpu/opcodes/0xFx.js
@@ -6,7 +6,7 @@ export default [
   { instruction: 'SBC', addressing: 'indirectIndex', cycles: 5 },
   /* TODO This is not implemented */
   { instruction: 'STP', addressing: 'implied', cycles: 3 },
-  /* TODO This is not implemented */
+  /* 0xf3: ISC indirectIndex */
   { instruction: 'ISC', addressing: 'indirectIndex', cycles: 8 },
   /* 0xf4: 2byte NOP */
   { instruction: 'NOP', addressing: 'zeropageX', cycles: 4 },
@@ -14,7 +14,7 @@ export default [
   { instruction: 'SBC', addressing: 'zeropageX', cycles: 4 },
   /* 0xf6: INC zeropageX */
   { instruction: 'INC', addressing: 'zeropageX', cycles: 6 },
-  /* TODO This is not implemented */
+  /* 0xf7: ISC zeropageX */
   { instruction: 'ISC', addressing: 'zeropageX', cycles: 6 },
   /* 0xf8: SED implied */
   { instruction: 'SED', addressing: 'implied', cycles: 2 },
@@ -22,7 +22,7 @@ export default [
   { instruction: 'SBC', addressing: 'absoluteY', cycles: 4 },
   /* 0xfa: NOP */
   { instruction: 'NOP', addressing: 'implied', cycles: 2 },
-  /* TODO This is not implemented */
+  /* 0xfb: ISC absoluteY */
   { instruction: 'ISC', addressing: 'absoluteY', cycles: 7 },
   /* 0xfc: 3byte NOP */
   { instruction: 'NOP', addressing: 'absoluteX', cycles: 4 },
@@ -30,6 +30,6 @@ export default [
   { instruction: 'SBC', addressing: 'absoluteX', cycles: 4 },
   /* 0xfe: INC absoluteX */
   { instruction: 'INC', addressing: 'absoluteX', cycles: 7 },
-  /* TODO This is not implemented */
+  /* 0xff: ISC absoluteX */
   { instruction: 'ISC', addressing: 'absoluteX', cycles: 7 }
 ]
