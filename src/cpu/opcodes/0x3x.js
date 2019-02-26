12381d6fb81f0c74b374b0a0444d15dcc036c2c2
zecoo <10446970+zecoo89@users.noreply.github.com>
2019-02-26 22:14:38 +0900

    Fix comments

--- src/cpu/opcodes/0x3x.js
+++ src/cpu/opcodes/0x3x.js
@@ -6,30 +6,30 @@ export default [
   { instruction: 'AND', addressing: 'indirectIndex', cycles: 5 },
   /* TODO This is not implemented */
   { instruction: 'STP', addressing: 'implied', cycles: 3 },
-  /* TODO This is not implemented */
+  /* 0x33: RLA indirectIndex */
   { instruction: 'RLA', addressing: 'indirectIndex', cycles: 8 },
   /* 0x34: 2byte NOP (Use zeropage for 2byte}*/
   { instruction: 'NOP', addressing: 'zeropage', cycles: 4 },
   /* 0x35: AND zeropageX */
   { instruction: 'AND', addressing: 'zeropageX', cycles: 4 },
-  /* 0x36 ROL zeropageX */
+  /* 0x36: ROL zeropageX */
   { instruction: 'ROL', addressing: 'zeropageX', cycles: 6 },
-  /* TODO This is not implemented */
+  /* 0x37: RLA zeropageX */
   { instruction: 'RLA', addressing: 'zeropageX', cycles: 6 },
   /* 0x38: SEC implied */
   { instruction: 'SEC', addressing: 'implied', cycles: 2 },
-  /* 0x39: AND absoluteY*/
+  /* 0x39: AND absoluteY */
   { instruction: 'AND', addressing: 'absoluteY', cycles: 4 },
   /* 0x3a: NOP */
   { instruction: 'NOP', addressing: 'implied', cycles: 2 },
-  /* TODO This is not implemented */
+  /* 0x3b: RLA absoluteY */
   { instruction: 'RLA', addressing: 'absoluteY', cycles: 7 },
-  /* 0x3c: 3byte NOP (Use absolute for 3byte}*/
+  /* 0x3c: 3byte NOP (Use absolute for 3byte} */
   { instruction: 'NOP', addressing: 'absolute', cycles: 4 },
   /* 0x3d: AND absoluteX */
   { instruction: 'AND', addressing: 'absoluteX', cycles: 4 },
   /* 0x3e: ROL absoluteX */
   { instruction: 'ROL', addressing: 'absoluteX', cycles: 7 },
-  /* TODO This is not implemented */
+  /* 0x3f: RLA absoluteX */
   { instruction: 'RLA', addressing: 'absoluteX', cycles: 7 }
 ]
