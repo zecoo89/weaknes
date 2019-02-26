6b3786463717c8e07e5fc3d8479f4efa08c8c03d
zecoo <10446970+zecoo89@users.noreply.github.com>
2019-02-26 22:29:59 +0900

    Fix comments

--- src/cpu/opcodes/0xCx.js
+++ src/cpu/opcodes/0xCx.js
@@ -6,7 +6,7 @@ export default [
   { instruction: 'CMP', addressing: 'indexIndirect', cycles: 6 },
   /* 0xc2: NOP*/
   { instruction: 'NOP', addressing: 'immediate', cycles: 2 },
-  /* TODO This is not implemented */
+  /* 0xc3: DCP indexIndirect */
   { instruction: 'DCP', addressing: 'indexIndirect', cycles: 8 },
   /* 0xc4: CPY zeropage*/
   { instruction: 'CPY', addressing: 'zeropage', cycles: 3 },
@@ -14,7 +14,7 @@ export default [
   { instruction: 'CMP', addressing: 'zeropage', cycles: 3 },
   /* 0xc6: DEC zeropage*/
   { instruction: 'DEC', addressing: 'zeropage', cycles: 5 },
-  /* TODO This is not implemented */
+  /* 0xc7: DCP zeropage */
   { instruction: 'DCP', addressing: 'zeropage', cycles: 5 },
   /* 0xc8: INY implied */
   { instruction: 'INY', addressing: 'implied', cycles: 2 },
@@ -30,6 +30,6 @@ export default [
   { instruction: 'CMP', addressing: 'absolute', cycles: 4 },
   /* 0xce: DEC absolute */
   { instruction: 'DEC', addressing: 'absolute', cycles: 6 },
-  /* TODO This is not implemented */
+  /* 0xcf: DCP absolute */
   { instruction: 'DCP', addressing: 'absolute', cycles: 6 }
 ]
